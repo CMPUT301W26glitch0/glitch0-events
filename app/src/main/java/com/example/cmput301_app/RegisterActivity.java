d66a53cab99b47206a3d035b241bcb943cd976bd
Ege Simsek <egesimsek18@gmail.com>
2026-03-09 17:29:09 -0600

    Implemented class files for Profile, Entrant, Organizer, Event, and LotteryPool - Javadoc comments included

--- app/src/main/java/com/example/cmput301_app/RegisterActivity.java
+++ app/src/main/java/com/example/cmput301_app/RegisterActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterActivity extends AppCompatActivity {
+public class    RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore mDb;
 
